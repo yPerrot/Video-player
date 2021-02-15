8ce813a65b7725945a1e07a4b322f736b7cbb5e9
unknown <y.perrot@outlook.fr>
2021-02-15 14:36:37 +0100

    Add App tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, waitForElement } from '@testing-library/react';
 import App from './App';
 
 beforeEach(() => {
@@ -59,3 +59,27 @@ test("renders without crashing", () => {
   const div = document.createElement("div");
   render(<App />, div);
 });
+
+test("does not contains video player after initial render", () => {
+  const { container } = render(<App />);
+  const videoPlayer = container.querySelector(`[class="Video-Player"]`);
+  expect(videoPlayer).toBeNull();
+});
+
+test("does not contains chat room after initial render", () => {
+  const { container } = render(<App />);
+  const chatRoom = container.querySelector(`[class="chat-room-container"]`);
+  expect(chatRoom).toBeNull();
+});
+
+// test("contains video player after async fetch", async () => {
+//   const { container } = render(<App />);
+//   const videoPlayer = await waitForElement(() => container.querySelector(`[class="Video-Player"]`));
+//   expect(videoPlayer).toBeInTheDocument();
+// });
+
+// test("contains chat room after async fetch", async () => {
+//   const { container } = render(<App />);
+//   const chatRoom = await waitForElement(() => container.querySelector(`[class="chat-room-container"]`));
+//   expect(chatRoom).toBeInTheDocument();
+// });
